08a319f65718280f86d0132c5fe9cc79e1eba522
varuni-dev <v.agarwal@veeam.com>
2025-11-07 12:45:29 +0530

    feat: added const and utility to construct bp path
    Signed-off-by: varuni-dev <v.agarwal@veeam.com>

---
+++ utils/const.go
@@ -0,0 +1,17 @@
+package utils
+
+// kanister App Names
+const (
+	KanisterAppRDSAuroraSnap   = "rds-aurora-snap"
+	KanisterAppRDSPostgresSnap = "rds-postgres-snap"
+	KanisterAppRDSPostgres     = "rds-postgres"
+	KanisterAppRDSPostgresDump = "rds-postgres-dump"
+	KanisterAppKafka           = "kafka"
+)
+
+// Blueprint Folder Names
+const (
+	BPFolderNameAWSRDSAuroraMySQL     = "aws-rds-aurora-mysql"
+	BPFolderNameAWSRDSPostgres        = "aws-rds-postgres"
+	BPFolderNameKafkaAdobeS3Connector = "kafka-adobe-s3-connector"
+)
---
+++ utils/utils.go
@@ -0,0 +1,27 @@
+// Package utils provides utility functions for blueprint path construction
+package utils
+
+import "fmt"
+
+// GetBlueprintPath constructs the path to the blueprint file based on app and blueprint names
+func GetBlueprintPath(app string, blueprintName string) string {
+	var blueprintFolder string
+
+	// If blueprintName is not provided, use app name as blueprint name
+	if blueprintName == "" {
+		blueprintName = app
+	}
+
+	switch app {
+	case KanisterAppRDSAuroraSnap:
+		blueprintFolder = BPFolderNameAWSRDSAuroraMySQL
+	case KanisterAppRDSPostgresSnap, KanisterAppRDSPostgres, KanisterAppRDSPostgresDump:
+		blueprintFolder = BPFolderNameAWSRDSPostgres
+	case KanisterAppKafka:
+		blueprintFolder = BPFolderNameKafkaAdobeS3Connector
+	default:
+		blueprintFolder = blueprintName
+	}
+
+	return fmt.Sprintf("%s/%s-blueprint.yaml", blueprintFolder, blueprintName)
+}
